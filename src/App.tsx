cb4c4e3d6151b40e95779754e85d3995b8b6f7d8
朱宇奇 <396838319@qq.com>
2022-03-21 11:31:27 +0800

    project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DatePicker } from 'antd'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <DatePicker/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
