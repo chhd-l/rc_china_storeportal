fdcde0a589f30d7164625456c08b18e5a600c031
ciloi <396838319@qq.com>
2022-04-22 14:01:44 +0800

    修改fetch调用dist文件

--- src/framework/api/fetcher.ts
+++ src/framework/api/fetcher.ts
@@ -1,4 +1,5 @@
-import ClientBuilder from '../../../rc-china-commerce/packages/fetch/lib'
+// import ClientBuilder from '../../../rc-china-commerce/packages/fetch/lib'
+import ClientBuilder from '../../../rc-china-commerce/packages/fetch'
 
 // const API_URL = 'http://localhost:9000/graphql'
 const API_URL = 'https://dtc-faas-dtc-plaform-dev-woyuxzgfcv.cn-shanghai.fcapp.run/graphql'
