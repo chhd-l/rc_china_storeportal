1df79b11c43935dd503769d0301bcf9d96094682
ciloi <396838319@qq.com>
2022-04-26 15:56:24 +0800

    引用npm包

--- src/framework/api/fetcher.ts
+++ src/framework/api/fetcher.ts
@@ -1,5 +1,5 @@
-import ClientBuilder from '../../../rc-china-commerce/packages/fetch/'
-// import ClientBuilder from '@/rc-china-commerce/packages/fetch/lib/index'
+import ClientBuilder from '@rc-china-commerce/fetch/'
+// import ClientBuilder from '/@/rc-china-commerce/packages/fetch/lib/index'
 
 // const API_URL = 'http://localhost:9000/graphql'
 const API_URL = 'https://dtc-faas-dtc-plaform-dev-woyuxzgfcv.cn-shanghai.fcapp.run/graphql'
