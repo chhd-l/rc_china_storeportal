a6b7da9ec290ed4e998cd5efa1e03b9d7389153e
tqx <1585224134@qq.com>
2022-04-26 22:23:27 +0800

    upd: Layout Adaptive change

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -2,25 +2,29 @@ import { Outlet } from "react-router-dom";
 import Menus from "../Menus";
 import TopHeader from "../Header";
 import { Layout } from "antd";
+import { important } from "tailwind.config";
 const { Header, Content, Sider } = Layout;
 const AppLayout = () => {
   return (
     <Layout>
       <TopHeader />
       <Layout style={{ marginTop: 64 }}>
+        <Layout style={{width:"16%",height: "calc(100vh - 64)",top: "58px",background:"#fff"}} className="overflow-auto fixed left-0 bottom-0">
         <Sider
-          width={"16%"}
+          width={"100%"}
           theme="light"
-          className="overflow-auto fixed left-0 bottom-0"
-          style={{
-            height: "calc(100vh - 64)",
-            top: "58px",
-          }}
+          style={{background:"#fff"}}
+          // className="overflow-auto fixed left-0 bottom-0"
+          // style={{
+          //   height: "calc(100vh - 64)",
+          //   top: "58px",
+          // }}
         >
           <Menus />
         </Sider>
-        <Layout style={{ marginLeft: 200 }}>
-          <Content>
+        </Layout>
+        <Layout style={{width:"84%",marginLeft:"auto"}}>
+          <Content >
             <Outlet />
           </Content>
         </Layout>
