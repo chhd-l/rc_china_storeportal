423e37b87b3c8eb7c2373d9c35a7e2bf5aef77f6
zuoqin <qzuo@deloitte.com.cn>
2022-04-29 09:59:02 +0800

    bug fixed

--- src/views/petOwnerDetail/index.tsx
+++ src/views/petOwnerDetail/index.tsx
@@ -67,24 +67,26 @@ const PetOwnerList = () => {
               <Pets id="pet-information" customerId={petOwnerId} />
               <TencentAccount id="tencent-account" customerId={petOwnerId} />
               <Orders id="order-information" orderList={orderList} />
-              <Subscriptions id="subscription-information" subscriptionList={subscriptionList} />
+              {/*<Subscriptions id="subscription-information" subscriptionList={subscriptionList} />*/}
               <Address id="my-address" customerId={petOwnerId} />
-              <Coupons id="coupon-information" couponCodeList={couponCodeList} />
-              <SmartDevice id="smart-device" smartDeviceList={smartDeviceList} />
+              {/*<Coupons id="coupon-information" couponCodeList={couponCodeList} />*/}
+              {/*<SmartDevice id="smart-device" smartDeviceList={smartDeviceList} />*/}
             </InfoContainer>
           ) : null}
         </div>
-        <div className="fixed right-0 rc-anchor">
+        <div className="fixed rc-anchor" style={{
+          right: '50px'
+        }}>
           <Anchor affix={true} offsetTop={120} className="petowner-anchor-link">
             <Link href="#basic-information" title="Basic Information" />
             <Link href="#tagging" title="Tagging" />
             <Link href="#pet-information" title="Pet Information" />
             <Link href="#tencent-account" title="Tencent Account" />
             <Link href="#order-information" title="Order Information" />
-            <Link href="#subscription-information" title="Subscription Information" />
+            {/*<Link href="#subscription-information" title="Subscription Information" />*/}
             <Link href="#my-address" title="My Address" />
-            <Link href="#coupon-information" title="Coupon Information" />
-            <Link href="#smart-device" title="Smart Device" />
+            {/*<Link href="#coupon-information" title="Coupon Information" />*/}
+            {/*<Link href="#smart-device" title="Smart Device" />*/}
           </Anchor>
         </div>
       </div>
