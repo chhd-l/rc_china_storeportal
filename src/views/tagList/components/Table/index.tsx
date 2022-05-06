549dc48e6ef2367c334aad44c4c3c100684b4f9f
zuoqin <qzuo@deloitte.com.cn>
2022-05-06 12:39:42 +0800

    tag list

---
+++ src/views/tagList/components/Table/index.tsx
@@ -0,0 +1,66 @@
+import { Avatar, Button, Switch, Table, Tooltip } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import React from 'react'
+import { Customer } from '@/framework/types/customer'
+
+interface PetOwnerTableProps {
+  petOwnerList: Customer[]
+}
+
+const Index = ({ petOwnerList }: PetOwnerTableProps) => {
+  const navigator = useNavigate()
+  const columns = [
+    {
+      title: 'Tagging Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Pet Owner(s)',
+      dataIndex: 'customerCount',
+      key: 'customerCount',
+    },
+    {
+      title: 'Tagging Status',
+      dataIndex: 'isEnabled',
+      key: 'isEnabled',
+      render: (text: any) => <Switch checked={text} onChange={() => {}} />,
+    },
+    {
+      title: 'Options',
+      key: 'Options',
+      render: (text: any, record: any) => (
+        <>
+          <Tooltip title="View Details">
+            <span
+              className="cursor-pointer iconfont icon-kjafg primary-color"
+              onClick={() => {
+                // navigator('/petOwner/edit-tag', { state: { id: record.id } })
+              }}
+            />
+          </Tooltip>
+          <Tooltip title="Delete">
+            <span className="cursor-pointer ml-2 iconfont icon-Frame3 text-red-500 text-xl" onClick={() => {}} />
+          </Tooltip>
+        </>
+      ),
+    },
+  ]
+  return (
+    <>
+      <div className="flex justify-end mb-4">
+        <Button
+          danger
+          onClick={() => {
+            // navigator('/account/add-account')
+          }}
+        >
+          + Add New Tag
+        </Button>
+      </div>
+      <Table dataSource={petOwnerList} columns={columns} rowKey="id" className="rc-table" pagination={false} />
+    </>
+  )
+}
+
+export default Index
