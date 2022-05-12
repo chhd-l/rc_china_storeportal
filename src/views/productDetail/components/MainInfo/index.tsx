a1fd8727807451b4ee6c83c293eeaf2a732fb824
lizongji <572510582@qq.com>
2022-05-12 16:15:58 +0800

    update

--- src/views/productDetail/components/MainInfo/index.tsx
+++ src/views/productDetail/components/MainInfo/index.tsx
@@ -179,7 +179,7 @@ const MainInfo: FC<MainInfoProps> = ({ cateInfo, showCatePop, children, beforeDa
           feedingDays: Number(values.feedingDays),
           isSupport100: values.isSupport100,
           id: detail.skuId,
-          defaultImage: 'https://miniapp-product.royalcanin.com.cn/rcmini2020/upload/1632987707399_z7bUuS.png',
+          // defaultImage: 'https://miniapp-product.royalcanin.com.cn/rcmini2020/upload/1632987707399_z7bUuS.png',
           goodsVariantBundleInfo: detail.goodsVariantBundleInfo?.map((el: any) => {
             let bundleInfo = {
               bundleNumber: el.bundleNumber,
