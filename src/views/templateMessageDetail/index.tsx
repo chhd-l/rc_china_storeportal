73dfa9cd51a10aa0578483da91850ced3fcfd294
zuoqin <qzuo@deloitte.com.cn>
2022-05-13 10:30:28 +0800

    bug fixed

--- src/views/templateMessageDetail/index.tsx
+++ src/views/templateMessageDetail/index.tsx
@@ -2,30 +2,18 @@ import { ContentContainer, InfoContainer } from '@/components/ui'
 import { TemplateMessageItemProps } from '@/framework/types/wechat'
 import ProForm, { ProFormInstance, ProFormSelect, ProFormText, ProFormTextArea } from '@ant-design/pro-form'
 import { Button, message } from 'antd'
-import Mock from 'mockjs'
 import { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router'
-import { messageList, templateContent } from '../templateMessageList/modules/mockdata'
 import './index.less'
 import { getTemplateDetail, updateTemplateMessage } from '@/framework/api/wechatSetting'
 import { useParams } from 'react-router-dom'
 import _ from 'lodash'
 
-const templateTitleList = Mock.mock(messageList).list
-const templateContentMock = Mock.mock(templateContent).list
-interface TemplateContentProps {
-  keyword?: string
-  display?: string
-  keywordDefault?: string
-}
-
 const TemplateMessageDetail = () => {
   const navigation = useNavigate()
   const [templateInfo, setTemplateInfo] = useState<TemplateMessageItemProps>({})
-  const [contentList, setContentList] = useState<TemplateContentProps[]>(templateContentMock)
   const formRef = useRef<ProFormInstance>()
   const params = useParams()
-  const contentListTitle = ['keyword name', 'Display', 'keyword default']
   let formItemLayout = { labelCol: { span: 6 }, wrapperCol: { span: 16 } }
 
   const getTemplateDetailInfo = async () => {
@@ -36,10 +24,10 @@ const TemplateMessageDetail = () => {
 
   const modifyTemplateMessage = async (value: any) => {
     console.log('1111', value)
-    const params=_.omit(value,['title','primaryIndustry','deputyIndustry'])
-    const res = await updateTemplateMessage(Object.assign(params,{id:templateInfo.id}))
+    const params = _.omit(value, ['title', 'primaryIndustry', 'deputyIndustry'])
+    const res = await updateTemplateMessage(Object.assign(params, { id: templateInfo.id }))
     if (res) {
-     navigation('/template/template-message-list')
+      navigation('/template/template-message-list')
     }
   }
 
@@ -51,7 +39,6 @@ const TemplateMessageDetail = () => {
     <>
       <ContentContainer className="template-message-detail pr-6">
         <InfoContainer className="">
-          {' '}
           {templateInfo.id ? (
             <ProForm
               className="text-right"
@@ -62,10 +49,6 @@ const TemplateMessageDetail = () => {
               onFinish={async (values) => {
                 console.log(values)
                 await modifyTemplateMessage(values)
-                // const val1 = await formRef.current?.validateFields()
-                // console.log('validateFields:', val1)
-                // const val2 = await formRef.current?.validateFieldsReturnFormatValue?.()
-                // console.log('validateFieldsReturnFormatValue:', val2)
                 message.success('提交成功')
               }}
               submitter={{
@@ -73,8 +56,6 @@ const TemplateMessageDetail = () => {
                   console.log(props)
                   return [
                     <Button
-                      // className=' text-white'
-                      // type='primary'
                       key="rest"
                       onClick={() => {
                         navigation(`/template/template-message-list`)
@@ -89,11 +70,6 @@ const TemplateMessageDetail = () => {
                 },
               }}
               formRef={formRef}
-              // params={{ id: '100' }}
-              // dateFormatter={(value, valueType) => {
-              //   console.log('---->', value, valueType)
-              //   return value.format('YYYY/MM/DD HH:mm:ss')
-              // }}
               request={async () => {
                 return {
                   name: '蚂蚁设计有限公司',
@@ -108,7 +84,7 @@ const TemplateMessageDetail = () => {
                   name="templateId"
                   label="Template ID"
                   placeholder="Please input Template ID"
-                  disabled 
+                  disabled
                 />
                 <ProFormSelect
                   colProps={{ span: 12 }}
@@ -129,7 +105,7 @@ const TemplateMessageDetail = () => {
                   name="title"
                   label="Template Name"
                   placeholder="Please input Template Name"
-                  disabled 
+                  disabled
                 />
                 <ProFormText
                   colProps={{ span: 12 }}
@@ -164,7 +140,7 @@ const TemplateMessageDetail = () => {
                 <ProFormText
                   colProps={{ span: 12 }}
                   name="pagepath"
-                  label="Mini Program Jump Path"
+                  label="Jump Path"
                   placeholder="Please input Mini Program Jump Path"
                 />
               </ProForm.Group>
@@ -188,34 +164,6 @@ const TemplateMessageDetail = () => {
           {`${templateInfo.example}`}
         </div>
       </InfoContainer>
-      {/* <div className='flex bg-gary1 mb-7'>
-        <InfoContainer>
-          <div className='bg-white'>
-            示例：
-            {`${templateInfo.example}`}
-          </div>
-        </InfoContainer>
-
-        <div className='flex-1 ml-6 bg-white'>
-          <InfoContainer>
-            <Row className='py-3 bg-gray1  px-4'>
-              {contentListTitle?.map(el => (
-                <Col key={`title-${el}`} span={4}>
-                  {el}
-                </Col>
-              ))}
-            </Row>
-
-            {contentList?.map(el => (
-              <Row className='py-3  px-4'>
-                <Col span={4}>{el.keyword}</Col>
-                <Col span={4}>{el.display}</Col>
-                <Col span={4}>{el.keywordDefault}</Col>
-              </Row>
-            ))}
-          </InfoContainer>
-        </div>
-      </div> */}
     </>
   )
 }
