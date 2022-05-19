9aaa3a28262dedf50a2a3a8c3ef42d2e4d15a6f8
zuoqin <qzuo@deloitte.com.cn>
2022-05-19 13:54:34 +0800

    vouchers

--- src/views/MarketingCentreList/index.tsx
+++ src/views/MarketingCentreList/index.tsx
@@ -8,6 +8,7 @@ const { Title } = Typography
 
 const MarketingCentreList = () => {
   const navigator = useNavigate()
+
   return (
     <ContentContainer className="bg-white px-8">
       <Title level={3}>Marketing Tools</Title>
@@ -17,18 +18,18 @@ const MarketingCentreList = () => {
           <div className="grid grid-cols-3 gap-4">
             {item.children.map((el: any) => (
               <div
+                style={{ backgroundColor: '#F9FAFB' }}
+                className="flex items-center justify-between p-4"
                 onClick={() => {
                   navigator(el.url)
                 }}
               >
-                <div style={{ backgroundColor: '#F9FAFB' }} className="flex items-center justify-between p-4">
-                  <IconFont className="m-auto" style={{ fontSize: '60px' }} type={el.icon} />
-                  <div className="flex flex-col justify-between w-4/5">
-                    <div className="mb-1">{el.title}</div>
-                    <span className="text-gray-500" style={{ fontSize: '10px', lineHeight: '14px' }}>
-                      {el.span}
-                    </span>
-                  </div>
+                <IconFont className="m-auto" style={{ fontSize: '60px' }} type={el.icon} />
+                <div className="flex flex-col justify-between w-4/5">
+                  <div className="mb-1">{el.title}</div>
+                  <span className="text-gray-500" style={{ fontSize: '10px', lineHeight: '14px' }}>
+                    {el.span}
+                  </span>
                 </div>
               </div>
             ))}
