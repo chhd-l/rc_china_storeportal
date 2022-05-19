5092478a2553f2d1a8f9855abf9127bc724bb2cb
zuoqin <qzuo@deloitte.com.cn>
2022-05-19 13:53:06 +0800

    voucher

--- src/views/MarketingCentreList/index.tsx
+++ src/views/MarketingCentreList/index.tsx
@@ -1,37 +1,42 @@
-import { Typography } from 'antd';
-import { ContentContainer } from "@/components/ui"
-import Card from "./components/Card";
-import { conter } from './modules/conten'
-const { Title } = Typography;
+import { Typography } from 'antd'
+import { ContentContainer } from '@/components/ui'
+import { PromotionTabList } from './modules/constants'
+import { useNavigate } from 'react-router-dom'
+import IconFont from '@/components/common/IconFont'
+
+const { Title } = Typography
 
 const MarketingCentreList = () => {
-
-    return (
-        <ContentContainer className="bg-white px-8">
-            <Title level={3}>Marketing Tools</Title>
-            <div>
-                <Title level={4}>Boost Sales with Promotion</Title>
-                <div className='flex items-center'>
-                    {
-                        conter.map((item) => (
-                            <Card title={item.title} span={item.span} icon={item.icon} backColor={item.backColor} />
-                        ))
-                    }
+  const navigator = useNavigate()
+  return (
+    <ContentContainer className="bg-white px-8">
+      <Title level={3}>Marketing Tools</Title>
+      {PromotionTabList.map((item) => (
+        <div className="mb-16">
+          <Title level={4}>{item.title}</Title>
+          <div className="grid grid-cols-3 gap-4">
+            {item.children.map((el: any) => (
+              <div
+                onClick={() => {
+                  navigator(el.url)
+                }}
+              >
+                <div style={{ backgroundColor: '#F9FAFB' }} className="flex items-center justify-between p-4">
+                  <IconFont className="m-auto" style={{ fontSize: '60px' }} type={el.icon} />
+                  <div className="flex flex-col justify-between w-4/5">
+                    <div className="mb-1">{el.title}</div>
+                    <span className="text-gray-500" style={{ fontSize: '10px', lineHeight: '14px' }}>
+                      {el.span}
+                    </span>
+                  </div>
                 </div>
-            </div>
-            <div className='mt-10 mb-48'>
-                <Title level={4}>Engage with Your Pet Owner</Title>
-                <div className='flex items-center'>
-                    <Card
-                        title='Live Streaming'
-                        span='Connect Live with your audience and answer shopper questions easily'
-                        icon='icon-a-LiveStreaming'
-                        backColor='#E5F5F4'
-                    />
-                </div>
-            </div>
-        </ContentContainer>
-    )
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </ContentContainer>
+  )
 }
 
 export default MarketingCentreList
