bc6ec18c55deb467f0312c249b8b59a7d9273290
chhd <xscnwan13@163.com>
2022-05-19 11:09:11 +0800

    MarketingCentreList新建页面

--- src/views/CreateNewVoucher/components/RuleSettings.tsx
+++ src/views/CreateNewVoucher/components/RuleSettings.tsx
@@ -5,10 +5,10 @@ const { Title } = Typography
 const RuleSettings = () => {
     const [isOpen, setIsopen] = useState(false)
     const [isOpen2, setIsopen2] = useState(false)
-    const [orices, setOrices] = useState('')
+    const [AmountOpen, setAmountOpen] = useState(true)
 
     return (
-        <div className="bg-white p-4">
+        <div className="bg-white p-4 RuleSettings">
             <Title className="mb-8" level={4}>
                 Rule Settings
             </Title>
@@ -26,26 +26,26 @@ const RuleSettings = () => {
             </Form.Item>
             <Form.Item
                 label="Discount Type | Amount"
-                wrapperCol={{ span: 12 }}
+                className={`${AmountOpen ? '' : 'mb-12'}`}
+                wrapperCol={{ span: 8 }}
                 shouldUpdate={(prevValues, curValues) => prevValues.DiscountType !== curValues.DiscountType}
                 required
-                rules={[
-                    {
-                        validator: (_, value) => {
-                            console.log('value',value)
-                            return value < 100000 ? Promise.resolve() : Promise.reject(new Error('Please note that the discount amount is > 60% of min basket price'))
-                        }
-                    },
-                ]}
             >
-                {({ getFieldValue }) => {
+                {({ getFieldValue, setFieldsValue }) => {
                     const DiscountType = getFieldValue('DiscountType')
+                    const MinimumBasketPrice = getFieldValue('Price')
                     return (
                         <div className="flex items-center border border-gray-300 border-solid">
                             <Form.Item name="DiscountType" initialValue="Fix Amount" className="m-0 h-8" wrapperCol={{ span: 'auto' }}>
                                 <Select
                                     className="Selectborder"
                                     placeholder='Select'
+                                    onChange={(v) => {
+                                        setAmountOpen(true)
+                                        setFieldsValue({
+                                            Amount: ''
+                                        })
+                                    }}
                                     options={[
                                         { lable: 'Fix Amount', value: 'Fix Amount' },
                                         { lable: 'By Percentage', value: 'By Percentage' },
@@ -55,18 +55,13 @@ const RuleSettings = () => {
                             {DiscountType !== 'By Percentage' ? (
                                 <>
                                     <span className="w-8 text-center border-l border-r">￥</span>
-                                    <Form.Item name="Amount" className="m-0 flex-1 h-8" rules={[
+                                    <Form.Item name="Amount" className="m-0 flex-1 h-8 Amount" rules={[
                                         {
                                             validator: (_, value) => {
-                                                return value < 100000 ? Promise.resolve() : Promise.reject(new Error('Please note that the discount amount is > 60% of min basket price'))
+                                                return value < (MinimumBasketPrice*0.6).toFixed(2) ? Promise.resolve() : Promise.reject(new Error('Please note that the discount amount is > 60% of min basket price'))
                                             },
                                             warningOnly: true,
                                         },
-                                        {
-                                            validator: (_, value) => {
-                                                return (value > 1) && (value < 99) ? Promise.resolve() : Promise.reject(new Error('Please enter a value between 1 and 99'))
-                                            }
-                                        }
                                     ]}>
                                         <InputNumber placeholder='Input' bordered={false} className="w-full" controls={false} />
                                     </Form.Item>
@@ -75,10 +70,28 @@ const RuleSettings = () => {
                                 <>
                                     <Form.Item
                                         name="Amount"
-                                        className="m-0 flex-1 h-8"
-                                        required
+                                        className="m-0 flex-1 h-8 Amount"
+                                        rules={[
+                                            {
+                                                validator: (_, value) => {
+                                                    return value < 60 ? Promise.resolve() : Promise.reject(new Error('Please note that the discount amount is > 60% of min basket price'))
+                                                },
+                                                warningOnly: true,
+                                            },
+                                            {
+                                                validator: (_, value) => {
+                                                    return (value > 0) && (value < 99) ? Promise.resolve() : Promise.reject(new Error('Please enter a value between 1 and 99'))
+                                                }
+                                            }
+                                        ]}
                                     >
-                                        <InputNumber controls={false} placeholder='Input' className="border-0 border-l rounded-none w-full" />
+                                        <InputNumber onChange={(v) => {
+                                            if(v < 99) {
+                                                setAmountOpen(true)
+                                            } else {
+                                                setAmountOpen(false)
+                                            }
+                                        }} controls={false} placeholder='Input' className="border-0 border-l rounded-none w-full" />
                                     </Form.Item>
                                     <span className="text-gray-400 w-10 text-center h-8 flex items-center justify-center border-l">
                                         %OFF
