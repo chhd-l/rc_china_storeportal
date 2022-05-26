9950256d18222eebdf842653e0c94163f6af2284
lizongji <572510582@qq.com>
2022-05-26 22:46:34 +0800

    update

--- src/framework/api/fetcher.ts
+++ src/framework/api/fetcher.ts
@@ -3,9 +3,9 @@ import ClientBuilder from '@rc-china-commerce/fetch/'
 import { message } from 'antd'
 // import ClientBuilder from '@/rc-china-commerce/packages/fetch/dist/fetch/lib'
 
-//const API_URL = 'http://localhost:9000/graphql'
+const API_URL = 'http://localhost:9000/graphql'
 // 生产
-const API_URL = 'https://msdev.d2cgo.com/faas/graphql'
+// const API_URL = 'https://msdev.d2cgo.com/faas/graphql'
 
 // 开发
 // const API_URL = 'https://dtc-faas-dev-dtc-plaform-dev-yfetifgpvj.cn-shanghai.fcapp.run/graphql'
