c5029507966354be9e8d6e7ff8f86a70d7c3a5b5
chhd <xscnwan13@163.com>
2022-05-31 10:11:31 +0800

    Intelligent Recommendation页面创建

--- src/views/intelligentRecommendation/components/Card.tsx
+++ src/views/intelligentRecommendation/components/Card.tsx
@@ -1,14 +1,16 @@
-import { UserOutlined } from '@ant-design/icons'
-import { Avatar, Button, Switch, Typography } from 'antd'
+import { Button, Switch, Typography } from 'antd'
 import { CardType } from '../modules/card'
 const { Title } = Typography
 
-const IntelligentRecommendation = ({ title, span, Enable, Edit, Details, open }: CardType) => {
+const IntelligentRecommendation = ({ title, span, Enable, Edit, Details, open, bgcImg }: CardType) => {
   return (
-    <div style={{ width: '32%', backgroundColor:'#F5F6F6', borderRadius:'4px' }} className="p-4 relative mt-4 ml-4">
-      <div className="flex h-24">
-        <div className="mr-2">
-          <Avatar size={64} icon={<UserOutlined />} />
+    <div
+      style={{ width: '32%', backgroundColor: '#F5F6F6', borderRadius: '4px' }}
+      className="p-5 pb-2 relative mt-4 ml-4"
+    >
+      <div className="flex h-28">
+        <div className="mr-3">
+          <div className={`CardImage ${bgcImg}`} />
         </div>
         <div>
           <Title className={`mb-1 text-sm ${Enable && 'w-44'}`}>
@@ -17,13 +19,11 @@ const IntelligentRecommendation = ({ title, span, Enable, Edit, Details, open }:
               <span className="cursor-pointer ml-1 iconfont icon-kjafg text-gray-300 text-xs" onClick={() => {}} />
             )}
           </Title>
-          <div style={{ fontSize: '12px', lineHeight:'1.2rem' }}>{span}</div>
+          <div style={{ fontSize: '12px', lineHeight: '1.2rem' }}>{span}</div>
         </div>
       </div>
       <div className="p-1 mt-1 flex">
-        <div className="flex-1 flex items-center">
-          { open && <Switch defaultChecked />}
-        </div>
+        <div className="flex-1 flex items-center">{open && <Switch defaultChecked onChange={(v) => open(v)} />}</div>
         <div className="flex-1 flex justify-end items-center">
           {Edit && (
             <span
--- src/views/intelligentRecommendation/modules/card.ts
+++ src/views/intelligentRecommendation/modules/card.ts
@@ -1,20 +1,25 @@
-export interface CardType {
+export type CardType = {
     title: string
-    children?: CardType[]
-    span?: string
-    Enable?: Function
-    Edit?: Function
-    Details?: Function
-    open?: Function
+    span: string
+    Enable?: Function | null
+    Edit?: Function | null
+    Details?: Function | null
+    open?: Function | null
+    bgcImg: string
+}
+export type CardTypeList = {
+    title: string
+    children: CardType[]
 }
 
-export const IntelligentRecommendationList: CardType[] = [
+export const IntelligentRecommendationList: CardTypeList[] = [
     {
         title: 'Recommended Basic Settings',
         children: [
             {
                 title: 'Product Recommendation Management',
                 span: 'Manually set weight for products or ban certain products',
+                bgcImg: 'RecommendationCard',
             },
         ],
     },
@@ -24,17 +29,21 @@ export const IntelligentRecommendationList: CardType[] = [
             {
                 title: 'Top Recommendation',
                 span: 'Sorting products according to the overall dimensional statistics, which is usually used in "Hot Sale".',
+                bgcImg: 'TopRecommendationCard',
             },
             {
                 title: 'Recommendation Based On Product Relativity',
+                bgcImg: 'BasedOnProductRelativityCard',
                 span: 'Recommend items related to the current item to enhance product co-sale. Commonly found in modules such as "Related Recommendations" on the product details page.',
             },
             {
                 title: 'Recommendation Based On Consumer Interest',
+                bgcImg: 'InterestyCardCard',
                 span: 'Provide custom recommendation service including recommending products based on user interests, which is often used in modules such as "you may also like".',
             },
             {
                 title: 'Product finder',
+                bgcImg: 'ProductfinderCard',
                 span: 'Provide custom recommendation service including recommending products based on subscription type and pet information, which is often used in modules such as "product finder".',
             },
         ],
@@ -44,34 +53,42 @@ export const IntelligentRecommendationList: CardType[] = [
         children: [
             {
                 title: 'Shopping Cart',
+                bgcImg: 'ShoppingCartCard',
                 span: 'Recommendation based on product relativity',
             },
             {
                 title: 'Product Detail',
+                bgcImg: 'ProductDetailCard',
                 span: 'Recommendation based on user interest',
             },
             {
                 title: 'Product List',
+                bgcImg: 'ProductListCard',
                 span: 'Top recommendation',
             },
             {
                 title: 'My Account',
+                bgcImg: 'MyAccountCard',
                 span: 'Recommendation based on user interest',
             },
             {
                 title: 'Member Center',
+                bgcImg: 'MemberCenterCard',
                 span: 'Recommendation based on user interest',
             },
             {
                 title: 'Order Confirmation Page',
                 span: 'Top recommendation',
+                bgcImg: 'OrderConfirmationPageCard',
             },
             {
                 title: 'Category',
+                bgcImg: 'CategoryCard',
                 span: 'Recommendation based on product relativity',
             },
             {
                 title: 'Category Recommendation Channels',
+                bgcImg: 'ChannelsCard',
                 span: 'Recommendation based on user interest',
             },
         ],
