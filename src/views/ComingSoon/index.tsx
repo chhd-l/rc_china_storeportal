d5f27e0c1e3142f4468a29f40a22651e73a9d7f4
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2022-06-09 16:14:34 +0800

    修改comming soon界面

--- src/views/ComingSoon/index.tsx
+++ src/views/ComingSoon/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import comingsoon from '@/assets/images/comingsoon.png'
-import { ContentContainer, InfoContainer } from '@/components/ui'
+import './index.less'
 
 const ComingSoon = () => {
   return (
-    <ContentContainer>
-      <InfoContainer>
-        <div className="flex justify-center items-center py-40">
-          <img src={comingsoon} alt="" className="comingsoon" />
-        </div>
-      </InfoContainer>
-    </ContentContainer>
+    <div className="comming-soon-container">
+      <img src={comingsoon} alt="" className="comingsoon" />
+    </div>
   )
 }
 export default ComingSoon
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,7 +3,7 @@ import comingsoon from "../assets/images/comingsoon.png"
 import "./index.less"
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       {/* <div className='text-xl'>Home</div>
       <div className='text-xs'>Home</div> */}
       <img src={comingsoon} alt="" className='comingsoon'/>
