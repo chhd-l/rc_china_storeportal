1c7631a6e2ab6668b6f11378cf3e19531f0176e6
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2022-06-09 18:44:39 +0800

    change api

--- src/framework/api/fetcher.ts
+++ src/framework/api/fetcher.ts
@@ -6,15 +6,15 @@ import { message } from 'antd'
 // const API_URL = 'http://localhost:9000/graphql'
 // 生产
 // const API_URL = 'http://20.62.176.70/faas/graphql'
-// const API_URL = 'https://msdev.d2cgo.com/faas/graphql'
-const API_URL = 'https://msstg.d2cgo.com/faas/graphql'
+const API_URL = 'https://msdev.d2cgo.com/faas/graphql'
+// const API_URL = 'https://msstg.d2cgo.com/faas/graphql'
 
 // 开发
 // const API_URL = 'https://dtc-faas-dev-dtc-plaform-dev-yfetifgpvj.cn-shanghai.fcapp.run/graphql'
 
 // const API_URL = `${process.env.REACT_APP_BASE_URL}/graphql`
 
-export const UPLOAD_API_URL = 'https://msstg.d2cgo.com/faas/upload'
+export const UPLOAD_API_URL = 'https://msdev.d2cgo.com/faas/upload'
 
 const ApiRoot = new ClientBuilder().config({
   url: API_URL,
