560b587dc2897b8f96d4a298ce5ae61432ee5eec
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2022-06-10 12:25:15 +0800

    更改faas层的地址

--- src/framework/api/fetcher.ts
+++ src/framework/api/fetcher.ts
@@ -4,10 +4,10 @@ import { message } from 'antd'
 import history from '@/routers/history'
 // import ClientBuilder from '@/rc-china-commerce/packages/fetch/dist/fetch/lib'
 
-// const API_URL = 'http://localhost:9000/graphql'
+// const API_URL = 'https://ms.wamdy.com/faas/graphql'
 // 生产
-// const API_URL = 'http://20.62.176.70/faas/graphql'
-const API_URL = 'https://msdev.d2cgo.com/faas/graphql'
+const API_URL = 'https://ms.wamdy.com/faas/graphql'
+// const API_URL = 'https://msdev.d2cgo.com/faas/graphql'
 // const API_URL = 'https://msstg.d2cgo.com/faas/graphql'
 
 // 开发
@@ -15,7 +15,7 @@ const API_URL = 'https://msdev.d2cgo.com/faas/graphql'
 
 // const API_URL = `${process.env.REACT_APP_BASE_URL}/graphql`
 
-export const UPLOAD_API_URL = 'https://msdev.d2cgo.com/faas/upload'
+export const UPLOAD_API_URL = 'https://ms.wamdy.com/faas/upload'
 
 const ApiRoot = new ClientBuilder().config({
   url: API_URL,
