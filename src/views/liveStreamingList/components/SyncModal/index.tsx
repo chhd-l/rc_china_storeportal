dd7b309a860b1b5eb3b3bd58b41e1367dc942a2f
zuoqin <qzuo@deloitte.com.cn>
2022-06-14 11:24:50 +0800

    sync livestreaming interaction

---
+++ src/views/liveStreamingList/components/SyncModal/index.tsx
@@ -0,0 +1,149 @@
+import { message, Modal, Pagination, Spin, Table } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { LiveStreaming } from '@/framework/types/liveStreaming'
+import moment from 'moment'
+import { getLiveStreamingList, syncLiveStreaming } from '@/framework/api/liveStreaming'
+import { handlePageParams } from '@/utils/utils'
+import { PageParamsProps } from '@/framework/types/common'
+import { initPageParams } from '@/lib/constants'
+
+const SyncModal = ({
+  syncTipModalShow,
+  closeSyncModal,
+  syncSuccess,
+}: {
+  syncTipModalShow: boolean
+  closeSyncModal: Function
+  syncSuccess: Function
+}) => {
+  const [liveStreamingList, setLiveStreamingList] = useState<LiveStreaming[]>([])
+  const [syncLoading, setSyncLoading] = useState(false)
+  const [pageParams, setPageParams] = useState<PageParamsProps>(initPageParams)
+  const [total, setTotal] = useState(0)
+  const { currentPage, pageSize } = pageParams
+  const [loading, setLoading] = useState(false)
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([])
+  const [selectedRows, setSelectedRows] = useState<any[]>([])
+  const columns = [
+    {
+      title: 'Live Streaming ID',
+      dataIndex: 'roomId',
+    },
+    {
+      title: 'Live Streaming Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Live Streaming Period',
+      dataIndex: 'period',
+      render: (text: any, record: any) => (
+        <span className="text-gray-400">
+          {moment(record.startTime).format('YYYY/MM/DD HH:mm')} - <br />
+          {moment(record.endTime).format('YYYY/MM/DD HH:mm')}
+        </span>
+      ),
+    },
+    {
+      title: 'Status',
+      dataIndex: 'liveStatus',
+      render: (text: any, record: any) => (
+        <span
+          className={`${
+            text === 101
+              ? 'bg-ongoingBg text-ongoingText'
+              : text === 102
+              ? 'bg-upcomingBg text-theme-red'
+              : 'bg-expiredBg'
+          } w-20 h-6 flex items-center justify-center`}
+        >
+          {text === 101 ? 'Ongoing' : text === 102 ? 'Upcoming' : 'Expired'}
+        </span>
+      ),
+    },
+  ]
+
+  const getLiveStreamingLists = async () => {
+    setLoading(true)
+    const res = await getLiveStreamingList({ ...handlePageParams(pageParams), isNeedTotal: true })
+    setLiveStreamingList(res.records)
+    setTotal(res.total)
+    setLoading(false)
+  }
+
+  const syncLiveStreams = async () => {
+    setSyncLoading(true)
+    const res = await syncLiveStreaming('000001')
+    if (res) {
+      message.success({ className: 'rc-message', content: 'Synchronize success' })
+    }
+    syncSuccess && syncSuccess()
+    setSyncLoading(false)
+  }
+
+  const changePage = (page: any, pageSize: any) => {
+    setPageParams({ currentPage: page, pageSize: pageSize })
+  }
+
+  useEffect(() => {
+    getLiveStreamingLists()
+  }, [pageParams])
+
+  useEffect(() => {
+    getLiveStreamingLists()
+  }, [])
+
+  return (
+    <>
+      <Modal
+        visible={syncTipModalShow}
+        width={920}
+        className="rc-modal"
+        title="Synchronize Live Streaming"
+        closable={false}
+        onCancel={() => closeSyncModal && closeSyncModal()}
+        onOk={() => syncLiveStreams()}
+        okText="Confirm"
+        confirmLoading={syncLoading}
+        destroyOnClose
+      >
+        <div className="mb-md">Please select the live Streaming you want to Synchronize</div>
+        {loading ? (
+          <div className="flex justify-center items-center h-80">
+            <Spin />
+          </div>
+        ) : (
+          <>
+            <Table
+              dataSource={liveStreamingList}
+              columns={columns}
+              rowKey="id"
+              className="rc-table"
+              pagination={false}
+              rowSelection={{
+                selectedRowKeys: selectedRowKeys,
+                onChange: (selectedRowKeys, selectedRows) => {
+                  setSelectedRowKeys(selectedRowKeys)
+                  setSelectedRows(selectedRows)
+                },
+              }}
+            />
+            {total > 0 && (
+              <div className="flex flex-row justify-end mt-md -mb-6">
+                <Pagination
+                  className="rc-pagination"
+                  current={currentPage}
+                  total={total}
+                  pageSize={pageSize}
+                  onChange={changePage}
+                  showSizeChanger={true}
+                />
+              </div>
+            )}
+          </>
+        )}
+      </Modal>
+    </>
+  )
+}
+
+export default SyncModal
