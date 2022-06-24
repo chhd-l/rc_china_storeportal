1b7490304d7a3cb5ac79fcb4b8e3fb428de6e2dd
jinglv <732796330@qq.com>
2022-06-24 18:23:25 +0800

    提交

--- src/framework/api/fetcher.ts
+++ src/framework/api/fetcher.ts
@@ -1,14 +1,14 @@
-// import ClientBuilder from '@rc-china-commerce/fetch/'
-import ClientBuilder from '@/rc-china-commerce/packages/fetch/lib/index'
+import ClientBuilder from '@rc-china-commerce/fetch/'
+// import ClientBuilder from '@/rc-china-commerce/packages/fetch/lib/index'
 import { message } from 'antd'
 import history from '@/routers/history'
 // import ClientBuilder from '@/rc-china-commerce/packages/fetch/dist/fetch/lib'
 
 let isShowingError = false
 
-const API_URL = 'http://localhost:9000/graphql'
+// const API_URL = 'http://localhost:9000/graphql'
 // 生产
-// const API_URL = 'https://msstg.fivefen.com/faas/graphql'
+const API_URL = 'https://msstg.fivefen.com/faas/graphql'
 // const API_URL = 'http://20.62.176.70/faas/graphql'
 // const API_URL = 'https://msstg.d2cgo.com/faas/graphql'
 
