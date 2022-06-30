e44b4acb6cc8da641e26ec0e712233655c93a6ee
chhd <xscnwan13@163.com>
2022-06-30 13:09:43 +0800

    bug修复

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,21 +1,22 @@
-
 import { useNavigate } from 'react-router'
 import './index.less'
 import { body } from './modules/content'
 
 const Dashboard = () => {
-    const navigator = useNavigate()
-    return <div className="dashboard flex items-center justify-between flex-wrap">
-        {
-            body.map((item: any, idx: number) => (
-                <div className='block' key={idx}>
-                    <div className={`${item.img} bgImg cursor-pointer`} onClick={() => navigator(item.url)}/>
-                    <div className='title'>{item.title}</div>
-                    <div className='span'>{item.span}</div>
-                </div>
-            ))
-        }
+  const navigator = useNavigate()
+  return (
+    <div className="dashboard">
+      <div className='ContentArea flex items-center justify-between flex-wrap m-auto'>
+        {body.map((item: any, idx: number) => (
+          <div className="block" key={idx}>
+            <div className={`${item.img} bgImg cursor-pointer`} onClick={() => navigator(item.url)} />
+            <div className="title">{item.title}</div>
+            <div className="span">{item.span}</div>
+          </div>
+        ))}
+      </div>
     </div>
+  )
 }
 
 export default Dashboard
