fbe06c1efbcb295ae66c81141ab833e748e01993
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2022-07-15 16:03:19 +0800

    add test env for stg

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -207,7 +207,7 @@ module.exports = function (webpackEnv) {
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
-      publicPath: process.env.NODE_ENV === 'production' ? 'https://cdnstg.fivefen.com/' : paths.publicUrlOrPath,
+      publicPath: process.env.NODE_ENV === 'production' ? 'https://cdnstg.fivefen.com/' : process.env.NODE_ENV === 'test' ? 'https://cdnstg.fivefen.com/test_assets/' : paths.publicUrlOrPath,
       // Point sourcemap entries to original disk location (format as URL on Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
         ? (info) => path.relative(paths.appSrc, info.absoluteResourcePath).replace(/\\/g, '/')
