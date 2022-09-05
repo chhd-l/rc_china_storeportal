8096d978d965187e31e96b4472718fbc1a467a08
chhd <xscnwan13@163.com>
2022-09-05 13:55:26 +0800

    InvoiceList接口联调

--- src/views/InvoiceList/components/SearchHeader/index.tsx
+++ src/views/InvoiceList/components/SearchHeader/index.tsx
@@ -1,48 +1,63 @@
 import { Button, Col, DatePicker, Form, Input, Row, Select } from 'antd'
+import moment from 'moment'
 
 const { Option } = Select
 const { RangePicker } = DatePicker
 
-const SearchHeader = () => {
+const SearchHeader = ({ setBody, getList }: { setBody: Function; getList: Function }) => {
+  const [form] = Form.useForm()
+
   const onFinish = (values: any) => {
+    for (const key in values) {
+      if (!values[key]) {
+        delete values[key]
+      }
+    }
+    if (values.time) {
+      values.addStartTime = moment(values.time[0]).utc()
+      values.addEndTime = moment(values.time[1]).utc()
+      delete values.time
+    }
+    setBody(values)
+    getList(values)
     console.log('Success:', values)
   }
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo)
+  const onReset = () => {
+    form.resetFields()
+    setBody({})
+    getList({})
   }
 
   return (
     <Form
       name="basic"
+      form={form}
       className="bg-white px-[24px] pb-[24px] mb-[24px]"
       labelCol={{ span: 7 }}
       wrapperCol={{ span: 12 }}
       onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
     >
       <Row justify="start" style={{ width: '100%' }} gutter={[0, 14]}>
         <Col span={11}>
-          <Form.Item label="Order Number" name="ccc">
+          <Form.Item label="Order Number" name="orderNum">
             <Input placeholder="Input" />
           </Form.Item>
         </Col>
         <Col span={11}>
-          <Form.Item label="Application Period" name="eee">
+          <Form.Item label="Application Period" name="time">
             <RangePicker className="w-full" />
           </Form.Item>
         </Col>
         <Col span={11}>
-          <Form.Item label="Invoice Type" name="aaa">
+          <Form.Item label="Invoice Type" name="invoiceType">
             <Select placeholder="Choose Invoice Type" allowClear>
-              <Option value="male">male</Option>
-              <Option value="female">female</Option>
-              <Option value="other">other</Option>
+              <Option value="增值税电子普通发票">增值税电子普通发票</Option>
             </Select>
           </Form.Item>
         </Col>
         <Col span={11}>
-          <Form.Item label="Invoice Title" name="xxx">
+          <Form.Item label="Invoice Title" name="invoiceTitle">
             <Input placeholder="Input" />
           </Form.Item>
         </Col>
@@ -51,7 +66,9 @@ const SearchHeader = () => {
             <Button className="ml-[17%]" type="primary" htmlType="submit">
               Search
             </Button>
-            <Button className="ml-[16px]">Reset</Button>
+            <Button className="ml-[16px]" onClick={onReset}>
+              Reset
+            </Button>
           </Form.Item>
         </Col>
       </Row>
